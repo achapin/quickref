6f751af9c7040458c4d3b589816b320f4b406ff5
Aaron Chapin <aaron.chapin@gmail.com>
2020-07-06 11:37:11 +0200

    Set up meta files for scripts

---
+++ Assets/Reference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Reference<T>
+{
+    private T reference;
+    public T Ref
+    {
+        get
+        {
+            if(reference == null)
+            {
+                reference = (T) Convert.ChangeType(QuickRef.GetReference(typeof(T)), typeof(T));
+            }
+            return reference;
+        }
+    }
+}
